1808cfb7f06e911db3951ac363b7afc2f26fe9eb
Javier Botero <javierbotero10271@gmail.com>
2020-10-26 16:39:07 -0500

    Fix typo in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   plugins: [new HtmlWebpackPlugin()],
   devtool: 'inline-source-map',
-  modules: {
+  module: {
     rules: [
       {
         test: /\.m?js$/,
@@ -17,7 +17,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env'],
           },
         },
       },
